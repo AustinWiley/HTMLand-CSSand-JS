5089c82fad3f37411b5c85dfac1fe90711675d8f
Austin WIley <wileybeer@gmail.com>
2019-08-19 14:01:11 -0600

    js

---
+++ jsone.js
@@ -0,0 +1,20 @@
+const jon = {
+    Name: "jon",
+    height: 72
+}
+
+const mike = {
+    Name: "Mike",
+    height: 71
+}
+
+const sarah = {
+    Name: "Sarah",
+    height: 73
+}
+
+let arr = [jon, mike, sarah]
+
+funciton tallest(arr) = {
+    for (i, )
+}
